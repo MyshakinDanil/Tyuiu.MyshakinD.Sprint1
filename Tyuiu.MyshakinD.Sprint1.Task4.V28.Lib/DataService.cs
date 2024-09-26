4a2dda51f551551b2e648839d63bd4fd36fc5e75
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2024-09-27 02:24:00 +0500

    Remarks

--- Tyuiu.MyshakinD.Sprint1.Task4.V28.Lib/DataService.cs
+++ Tyuiu.MyshakinD.Sprint1.Task4.V28.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.MyshakinD.Sprint1.Task4.V28.Lib
     {
         public double Calculate(double x, double y)
         {
-            var res = Math.Cos(60 * Math.PI / 2) / Math.Exp(2 * x + y);
+            var res = Math.Round(Math.Cos(60 * Math.PI / 2) / Math.Exp(2 * x + y), 3);
             return res;
         }
     }
